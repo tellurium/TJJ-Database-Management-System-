6f4d128c99dd236c071ded9da212efa4817db3e9
tellurium <huairenpp@gmail.com>
2011-07-05 23:13:24 +0800

    ------T_T again

---
+++ WEB-INF/src/java/the/web/actions/InflateSubsystemList.java
@@ -0,0 +1,24 @@
+package the.web.actions;
+
+import java.util.ArrayList;
+
+public class InflateSubsystemList {
+	
+	private ArrayList<String> subsystemList;
+
+	public String execute() {
+		return "success";
+	}
+
+	public InflateSubsystemList() {
+	}
+
+	public ArrayList<String> getSubsystemList() {
+		return this.subsystemList;
+	}
+	
+	public void setSubsystemList(ArrayList<String> subsystemList) {
+		this.subsystemList = subsystemList;    	
+	}
+	
+}
